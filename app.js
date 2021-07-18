35c56117c4e78e41959aa0f286ea1a6a89b67c29
unknown <thomas.vassallo@gmail.com>
2021-07-18 23:07:23 +0100

    changed images and animation

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
-timeline.to('.birbs', 3, {x:-100})
-.fromTo('.fuji', 3, {y:20},{y:-150}, '-=3')
-.fromTo('.bgPrime', 3, {y:-70}, {y:0},'-=3')
-.to('.content', 3, {top:'0%'}, '-=3');
+timeline
+.fromTo('.moon', 3, {y:-10},{y:350}, '-=3')
+.fromTo('.moon2', 3, {y:30},{y:-250}, '-=3')
+.fromTo('.bgPrime', 3, {y:-150}, {y:-50},'-=3')
+.to('.content', 3, {top:'0%'}, '-=3')
+.fromTo('.boat', 3, {x:200}, {x:-150}, '-=3')
+.fromTo('.main-title', {opacity:1}, {opacity:0, duration:1.5}, '-=3');
+
 
 let scene = new ScrollMagic.Scene({
     triggerElement: "section",
@@ -14,3 +18,11 @@ let scene = new ScrollMagic.Scene({
 .setTween(timeline)
 .setPin("section")
 .addTo(controller);
+
+const boat = document.querySelector('.boat');
+
+if(boat.style.transform >= "translate(100px, 0px)"){
+    boat = boat.style.overflow-x == "visible";
+    boat.style.clip == "rect(100px,330px,100px,100px)";
+}
+
